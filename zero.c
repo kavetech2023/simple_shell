4d360e86618ff67e352234e41eb45f3be7898286
kavetech2023 <you@example.com>
2023-09-20 11:46:09 -0700

    zero

---
+++ zero.c
@@ -0,0 +1,12 @@
+/**
+ * This function prints the message "Hello, world!" to the console.
+ */
+void hello_world(void) {
+  printf("Hello, world!\n");
+}
+
+int main(void) {
+  hello_world();
+
+  return 0;
+}
